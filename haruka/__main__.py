cbba8a2758f12b21010d6856148ad1a4f503517a
ItsAviral <60238390+TheIncarnate@users.noreply.github.com>
2020-03-03 13:32:07 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -138,11 +138,11 @@ def send_start(bot, update):
     text = "Hey there! My name is Lucy - I'm here to help you manage your groups!\n\
 Click Help button to find out more about how to use me to my full potential.\n\n"
 
-    text += "Join [Support Group](https://Lucysupportchat) ( @lucysupportchat ) if you need any support or help\n\n\
-Follow [Lucy Updates](https://Lucysupportchat) ( @Lucysupportchat ) if you want to keep up with the news, updates and bot downtime!\n\n\
-Made with love by @ItsAviral\n\nWant to add me to your group? [Click here!](t.me/lucy_roBot?startgroup=true)"
+    text += "Join [Support Group](https://Lucysupportchat) ( @LucySupportChat ) if you need any support or help\n\n\
+Follow [Lucy Updates](https://Lucysupportchat) ( @LucySupportChat ) if you want to keep up with the news, updates and bot downtime!\n\n\
+Made with love by @ItsAviral and @Okay_retard\n\nWant to add me to your group? [Click here!](t.me/lucy_roBot?startgroup=true)"
 
-    keyboard = [[InlineKeyboardButton(text="📢 Support Group", url="https://t.me/lucysupportchat")]]
+    keyboard = [[InlineKeyboardButton(text="📢 Support Group", url="https://t.me/LucySupportChat")]]
     keyboard += [[InlineKeyboardButton(text="🛠 Control panel", callback_data="cntrl_panel_M")]]
     keyboard += [[InlineKeyboardButton(text="🇺🇸 Language", callback_data="set_lang_"), 
         InlineKeyboardButton(text="❔ Help", callback_data="help_back")]]
