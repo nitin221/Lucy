eb56f46e72e38df26d956bf5639997ea06fbd935
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-03-07 23:19:23 +0530

    Add files via upload

---
+++ haruka/modules/eval.py
@@ -0,0 +1,158 @@
+import logging
+import sys
+from contextlib import contextmanager, redirect_stdout
+from telegram import Update, Bot, ParseMode
+from telegram.ext import run_async
+
+
+from telegram.error import TimedOut, NetworkError
+from telegram import ParseMode
+
+from haruka.modules.helper_funcs.filters import CustomFilters
+from haruka import dispatcher, OWNER_ID, SUDO_USERS, SUPPORT_USERS
+from telegram.ext import CommandHandler, run_async, Filters, Updater
+from haruka.modules.disable import DisableAbleCommandHandler
+from telegram.ext.dispatcher import run_async
+from haruka.modules.helper_funcs.chat_status import bot_admin, can_promote, user_admin, can_pin
+from haruka import dispatcher, LOGGER
+
+from requests import get
+
+# Common imports for eval
+import sys
+import inspect
+import os
+import shutil
+import glob
+import math
+import textwrap
+import os
+import requests
+import json
+import gc
+import datetime
+import time
+import traceback
+import re
+import io
+import asyncio
+import random
+import subprocess
+import urllib
+import psutil
+
+namespaces = {}
+
+def namespace_of(chat, update, bot):
+    if chat not in namespaces:
+        namespaces[chat] = {
+            '__builtins__': globals()['__builtins__'],
+            'bot': bot,
+            'effective_message': update.effective_message,
+            'effective_user': update.effective_user,
+            'effective_chat': update.effective_chat,
+            'update': update
+        }
+
+    return namespaces[chat]
+
+
+def log_input(update):
+    user = update.effective_user.id
+    chat = update.effective_chat.id
+    LOGGER.info("IN: {} (user={}, chat={})".format(update.effective_message.text, user, chat))
+
+def send(msg, bot, update):
+    LOGGER.info("OUT: '{}'".format(msg))
+    bot.send_message(chat_id=update.effective_chat.id, text="`{}`".format(msg), parse_mode=ParseMode.MARKDOWN)
+
+
+@run_async
+def evaluate(bot, update):
+    send(do(eval, bot, update), bot, update)
+
+
+@run_async
+def execute(bot, update):
+    send(do(exec, bot, update), bot, update)
+
+def cleanup_code(code):
+    if code.startswith('```') and code.endswith('```'):
+        return '\n'.join(code.split('\n')[1:-1])
+    return code.strip('` \n')
+
+
+def do(func, bot, update):
+    log_input(update)
+    content = update.message.text.split(' ', 1)[-1]
+    body = cleanup_code(content)
+    env = namespace_of(update.message.chat_id, update, bot)
+
+    os.chdir(os.getcwd())
+    with open('%s/haruka/modules/helper_funcs/temp.txt' % os.getcwd(), 'w') as temp:
+        temp.write(body)
+
+    stdout = io.StringIO()
+
+    to_compile = 'def func():\n{}'.format(textwrap.indent(body, "  "))
+
+    try:
+        exec(to_compile, env)
+    except Exception as e:
+        return '{}: {}'.format(e.__class__.__name__, e)
+
+    func = env['func']
+
+    try:
+        with redirect_stdout(stdout):
+            func_return = func()
+    except Exception as e:
+        value = stdout.getvalue()
+        return '{}{}'.format(value, traceback.format_exc())
+    else:
+        value = stdout.getvalue()
+        result = None
+        if func_return is None:
+            if value:
+                result = '{}'.format(value)
+            else:
+                try:
+                    result = '{}'.format(repr(eval(body, env)))
+                except:
+                    pass
+        else:
+            result = '{}{}'.format(value, func_return)
+        if result:
+            if len(str(result)) > 2000:
+                result = 'Output is too long'
+            return result
+
+
+@run_async
+def clear(bot, update):
+    log_input(update)
+    global namespaces
+    if update.message.chat_id in namespaces:
+        del namespaces[update.message.chat_id]
+    send("Cleared locals.", bot, update)
+
+
+def error_callback(bot, update, error):
+    try:
+        raise error
+    except (TimedOut, NetworkError):
+        log.debug(error, exc_info=True)
+    except:
+        log.info(error, exc_info=True)
+
+__mod_name__ = "Eval Module"
+
+
+EVAL_HANDLER = CommandHandler("eval", evaluate, filters=CustomFilters.sudo_filter)
+exec_handle = CommandHandler(["x", "ex", "exe", "exec", "py"], execute, filters=CustomFilters.sudo_filter)
+clear_handle = CommandHandler('clearlocals', clear)
+
+dispatcher.add_handler(EVAL_HANDLER)
+dispatcher.add_handler(exec_handle)
+dispatcher.add_handler(clear_handle)
+#To be uncommented after testing
