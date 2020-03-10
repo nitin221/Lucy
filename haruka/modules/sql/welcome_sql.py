4a9e585ebd2d40210b33d1ad333db752a101dd98
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-03-10 17:59:45 +0530

    Update welcome_sql.py

--- haruka/modules/sql/welcome_sql.py
+++ haruka/modules/sql/welcome_sql.py
@@ -201,7 +201,26 @@ def welcome_mutes(chat_id):
     finally:
         SESSION.close()
 
+def clean_service(chat_id: Union[str, int]) -> bool:
+    try:
+        chat_setting = SESSION.query(CleanServiceSetting).get(str(chat_id))
+        if chat_setting:
+            return chat_setting.clean_service
+        return False
+    finally:
+        SESSION.close()
+        
 
+def set_clean_service(chat_id: Union[int, str], setting: bool):
+    with CS_LOCK:
+        chat_setting = SESSION.query(CleanServiceSetting).get(str(chat_id))
+        if not chat_setting:
+            chat_setting = CleanServiceSetting(chat_id)
+
+        chat_setting.clean_service = setting
+        SESSION.add(chat_setting)
+        SESSION.commit()
+        
 def set_welcome_mutes(chat_id, welcomemutes):
     with WM_LOCK:
         prev = SESSION.query(WelcomeMute).get((str(chat_id)))
