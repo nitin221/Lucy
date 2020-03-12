f004128ad68c4147af96938e2eeb1d421ef08dde
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-03-13 01:18:21 +0530

    Add files via upload

---
+++ haruka/__init__.py
@@ -0,0 +1,161 @@
+import logging
+import os
+import sys
+
+import telegram.ext as tg
+
+# enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO)
+
+LOGGER = logging.getLogger(__name__)
+
+# if version < 3.6, stop bot.
+if sys.version_info[0] < 3 or sys.version_info[1] < 6:
+    LOGGER.error("You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting.")
+    quit(1)
+
+ENV = bool(os.environ.get('ENV', False))
+
+if ENV:
+    TOKEN = os.environ.get('TOKEN', None)
+    try:
+        OWNER_ID = int(os.environ.get('OWNER_ID', None))
+    except ValueError:
+        raise Exception("Your OWNER_ID env variable is not a valid integer.")
+
+    MESSAGE_DUMP = os.environ.get('MESSAGE_DUMP', None)
+    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", None)
+
+    try:
+        SUDO_USERS = set(int(x) for x in os.environ.get("SUDO_USERS", "").split())
+    except ValueError:
+        raise Exception("Your sudo users list does not contain valid integers.")
+
+    try:
+        SUPPORT_USERS = set(int(x) for x in os.environ.get("SUPPORT_USERS", "").split())
+    except ValueError:
+        raise Exception("Your support users list does not contain valid integers.")
+
+    
+    try:
+        WHITELIST_USERS = set(int(x) for x in os.environ.get("WHITELIST_USERS", "").split())
+    except ValueError:
+        raise Exception("Your whitelisted users list does not contain valid integers.")
+
+
+
+    
+    WEBHOOK = bool(os.environ.get('WEBHOOK', False))
+    URL = os.environ.get('URL', "")  # Does not contain token
+    PORT = int(os.environ.get('PORT', 5000))
+    CERT_PATH = os.environ.get("CERT_PATH")
+
+    DB_URI = os.environ.get('DATABASE_URL')
+    DONATION_LINK = os.environ.get('DONATION_LINK')
+    LOAD = os.environ.get("LOAD", "").split()
+    NO_LOAD = os.environ.get("NO_LOAD", "").split()
+    DEL_CMDS = bool(os.environ.get('DEL_CMDS', False))
+    STRICT_ANTISPAM = bool(os.environ.get('STRICT_ANTISPAM', False))
+    WORKERS = int(os.environ.get('WORKERS', 8))
+    BAN_STICKER = os.environ.get('BAN_STICKER', 'CAADBQADTQEAAupFXico2ds1vUzopRYE')
+    STRICT_GMUTE = False
+    STRICT_GBAN = False
+    TEMPORARY_DATA = os.environ.get('TEMPORARY_DATA', None)
+    ALLOW_EXCL = os.environ.get('ALLOW_EXCL', False)
+    GBAN_LOGS = os.environ.get('GBAN_LOGS', "")
+    SPAMMERS =os.environ.get('SPAMMERS', "")
+
+else:
+    from haruka.config import Development as Config
+    TOKEN = Config.API_KEY
+    try:
+        OWNER_ID = int(Config.OWNER_ID)
+    except ValueError:
+        raise Exception("Your OWNER_ID variable is not a valid integer.")
+
+    MESSAGE_DUMP = Config.MESSAGE_DUMP
+    OWNER_USERNAME = Config.OWNER_USERNAME
+
+    try:
+        SUDO_USERS = set(int(x) for x in Config.SUDO_USERS or [])
+    except ValueError:
+        raise Exception("Your sudo users list does not contain valid integers.")
+
+    try:
+        SUPPORT_USERS = set(int(x) for x in Config.SUPPORT_USERS or [])
+    except ValueError:
+        raise Exception("Your support users list does not contain valid integers.")
+
+    try:
+        WHITELIST_USERS = set(int(x) for x in Config.WHITELIST_USERS or [])
+    except ValueError:
+        raise Exception("Your whitelisted users list does not contain valid integers.")
+
+    WEBHOOK = Config.WEBHOOK
+    URL = Config.URL
+    PORT = Config.PORT
+    CERT_PATH = Config.CERT_PATH
+
+    DB_URI = Config.SQLALCHEMY_DATABASE_URI
+    DONATION_LINK = Config.DONATION_LINK
+    LOAD = Config.LOAD
+    NO_LOAD = Config.NO_LOAD
+    DEL_CMDS = Config.DEL_CMDS
+    STRICT_ANTISPAM = Config.STRICT_ANTISPAM
+    WORKERS = Config.WORKERS
+    BAN_STICKER = Config.BAN_STICKER
+    TEMPORARY_DATA = Config.TEMPORARY_DATA
+    ALLOW_EXCL = Config.ALLOW_EXCL
+    GBAN_LOGS = Config.GBAN_LOGS
+    SPAMMERS = Config.SPAMMERS
+
+SUDO_USERS.add(OWNER_ID)
+
+
+
+
+updater = tg.Updater(TOKEN, workers=WORKERS)
+
+dispatcher = updater.dispatcher
+
+SUDO_USERS = list(SUDO_USERS)
+WHITELIST_USERS = list(WHITELIST_USERS)
+SUPPORT_USERS = list(SUPPORT_USERS)
+SPAMMERS = list(SPAMMERS)
+
+# Load at end to ensure all prev variables have been set
+from haruka.modules.helper_funcs.handlers import CustomCommandHandler, CustomRegexHandler
+
+# make sure the regex handler can take extra kwargs
+tg.RegexHandler = CustomRegexHandler
+
+if ALLOW_EXCL:
+    tg.CommandHandler = CustomCommandHandler
+
+try:
+	from haruka.antispam import antispam_restrict_user, antispam_cek_user, detect_user
+	antispam_module = True
+except ModuleNotFoundError:
+	antispam_module = False
+	LOGGER.info("Note: Can't load antispam module. This is an optional.")
+
+
+def spamfilters(text, user_id, chat_id, message):
+	# If msg from self, return True
+	if user_id == 1002584093:
+		return False
+	print("{} | {} | {} | {}".format(text, user_id, message.chat.title, chat_id))
+	if antispam_module:
+		parsing_date = time.mktime(message.date.timetuple())
+		detecting = detect_user(user_id, chat_id, message, parsing_date)
+		if detecting:
+			return True
+		antispam_restrict_user(user_id, parsing_date)
+	if int(user_id) in SPAMMERS:
+		print("This user is spammer!")
+		return True
+	else:
+		return False
+
