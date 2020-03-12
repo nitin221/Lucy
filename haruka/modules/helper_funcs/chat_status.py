16632a82c851cfa25ba81da9064318c5182b7aa5
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-03-13 01:57:54 +0530

    Update chat_status.py

--- haruka/modules/helper_funcs/chat_status.py
+++ haruka/modules/helper_funcs/chat_status.py
@@ -1,7 +1,7 @@
 from functools import wraps
 from typing import Optional
 
-from telegram import User, Chat, ChatMember, Update, Bot,
+from telegram import User, Chat, ChatMember, Update, Bot
 
 from haruka import DEL_CMDS, SUDO_USERS, WHITELIST_USERS, DEV_USERS
 import haruka.modules.sql.admin_sql as admin_sql
