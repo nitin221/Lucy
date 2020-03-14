d8c7303dddcfda68ac7364a8acfb502a05d2ac9d
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-03-14 17:14:18 +0530

    Update misc.py

--- haruka/modules/misc.py
+++ haruka/modules/misc.py
@@ -21,7 +21,7 @@ from telegram.utils.helpers import escape_markdown, mention_html
 
 from haruka import dispatcher, OWNER_ID, SUDO_USERS, SUPPORT_USERS, WHITELIST_USERS, BAN_STICKER, DEV_USERS
 from haruka.__main__ import GDPR
-from haruka.__main__ import STATS, USER_INFO
+from haruka.__main__ import STATS, USER_INFO, TOKEN
 from haruka.modules.disable import DisableAbleCommandHandler
 from haruka.modules.helper_funcs.extraction import extract_user
 from haruka.modules.helper_funcs.filters import CustomFilters
